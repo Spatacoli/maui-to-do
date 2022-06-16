ab00ca1a97d3d2e6b442398fe5939b2650a9a32b
Todd Spatafore <todd@thetodd.com>
2022-06-16 13:09:31 -0700

    New Repository for adding items

--- maui-to-do/ViewModels/MainViewModel.cs
+++ maui-to-do/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using maui_to_do.Models;
+using maui_to_do.Services;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
@@ -10,34 +11,36 @@ public partial class MainViewModel : ObservableObject
 {
     public ObservableCollection<ActionItem> Items { get; } = new();
 
-    [RelayCommand]
-    void GetActionItems()
-    {
-        if (Items.Count != 0) return;
+    [ObservableProperty]
+    private string title;
 
-        Items.Add(new ActionItem
+    [ObservableProperty]
+    private bool isComplete;
+
+    private ActionItemRepository Repo;
+
+    public MainViewModel(ActionItemRepository air)
+    {
+        Repo = air;
+    }
+
+    [RelayCommand]
+    async void GetActionItems()
+    {
+        Items.Clear();
+        var ai = await Repo.GetAllActionItemsAsync();
+
+        foreach (var item in ai)
         {
-            ID = 0,
-            Title = "Install .NET MAUI",
-            IsCompleted = true,
-        });
-        Items.Add(new ActionItem
-        {
-            ID = 1,
-            Title = "Start a Demo App",
-            IsCompleted = true,
-        });
-        Items.Add(new ActionItem
-        {
-            ID = 2,
-            Title = "Make To-Do app",
-            IsCompleted = false,
-        });
-        Items.Add(new ActionItem
-        {
-            ID = 3,
-            Title = "Get To-Do working on Android, iOS, and Mac",
-            IsCompleted = false,
-        });
+            Items.Add(item);
+        }
+    }
+
+    [RelayCommand]
+    async void AddActionItem()
+    {
+        await Repo.AddNewActionItemAsync(Title, IsComplete);
+        Title = string.Empty;
+        IsComplete = false;
     }
 }
